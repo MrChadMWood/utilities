24d73661ee5e144a5cce8413897b61befe2e253c
Chad Wood <mr.chadmwood@gmail.com>
2024-05-22 15:21:50 -0700

    Update psycopg2_multipurpose_executor.py

--- psycopg2_multipurpose_executor.py
+++ psycopg2_multipurpose_executor.py
@@ -10,24 +10,28 @@ def psql_exec(q, db_params=None, conn=None, return_data=False, q_params: tuple =
     q : str
         The SQL query to be executed.
     db_params : dict, optional
-        Dictionary containing parameters for establishing a database connection
-        (default is None).
+        Dictionary containing parameters for establishing a database connection.
+        Must include keys such as 'dbname', 'user', 'password', and 'host'.
+        Default is None.
     conn : psycopg2.extensions.connection, optional
-        Existing database connection object (default is None). If not provided,
-        a new connection will be established using `db_params`.
+        Existing database connection object. If not provided, a new connection
+        will be established using `db_params`. Default is None.
     return_data : bool, optional
-        Indicates whether the query returns a dataset (default is False). If True,
-        the result will be a list of dictionaries where each dictionary represents
-        a row of data with keys corresponding to the column names. If False, the result
-        will be the return value of the `cursor.execute` method.
+        Indicates whether the query returns a dataset. Default is False.
+        If True, the result will be a list of dictionaries where each dictionary
+        represents a row of data with keys corresponding to the column names.
+        If False, the result will be the return value of the `cursor.execute` method.
+    q_params : tuple, optional
+        Parameters to be used in the SQL query. Default is an empty tuple.
 
     Returns:
     list of dict or any : The result of the query execution.
         If `return_data` is True, returns a list of dictionaries where each dictionary
-        represents a row of data with keys corresponding to the column names. If `return_data`
-        is False, returns the return value of the `cursor.execute` method.
+        represents a row of data with keys corresponding to the column names.
+        If `return_data` is False, returns the return value of the `cursor.execute` method.
 
     Raises:
+    ValueError: If neither `db_params` nor `conn` is provided.
     Exception: If any error occurs during query execution, an exception is raised.
     """
     close_conn = False
